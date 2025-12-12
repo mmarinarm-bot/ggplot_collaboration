7c6ef43eb76f62282a3406de070d43615c0c7895
samdossa <komiri.dossa@ucad.edu.sn>
2025-12-12 08:08:49 -0400

    edit 8

--- script1.R
+++ script1.R
@@ -2,5 +2,6 @@ library(ggplot2)
 
 ggplot(data = iris, aes(Petal.Length, Petal.Width))+
   geom_point()+
-labs(title = "Iris morphometrics")
+labs(title = "Iris morphometrics") +
+  theme_minimal()
   
